0441d052ac0d8d2c7a3e9597716c14970ef8d391
nielsv <niels.vandekeybus@tenforce.com>
2014-01-30 07:59:44 +0100

    added binary dependencies in /lib

---
+++ lib/install.sh
@@ -0,0 +1,4 @@
+mvn install:install-file -Dfile=./.jar -DgroupId=at.punkt.commons -DartifactId=punkt-commons-vocabulary -Dversion=1.2.1 -Dpackaging=jar
+mvn install:install-file -Dfile=./.jar -DgroupId=at.punkt.poolparty.extractor -DartifactId=poolparty-extractor-client -Dversion=4.1.0-SNAPSHOT -Dpackaging=jar
+
+
