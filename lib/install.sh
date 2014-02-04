0e6282f15783f1bf71181fd9bb07b3de20b5f507
Christian Mader <mail@christianmader.net>
2014-02-04 15:32:01 +0100

    added correct transformer class, removed lib folder that should not be public

--- lib/install.sh
+++
@@ -1,7 +0,0 @@
-mvn install:install-file -Dfile=./poolparty-extractor-client-4.1.0-SNAPSHOT.jar  -DgroupId=at.punkt.poolparty.extractor -DartifactId=poolparty-extractor-client -Dversion=4.1.0-SNAPSHOT -Dpackaging=jar
-mvn install:install-file -Dfile=./punkt-commons-vocabulary-1.2.1.jar -DgroupId=at.punkt.commons -DartifactId=punkt-commons-vocabulary -Dversion=1.2.1 -Dpackaging=jar
-mvn install:install-file -Dfile=./poolparty-extractor-web-api-4.1.0-SNAPSHOT.jar -DgroupId=at.punkt.poolparty.extractor -DartifactId=poolparty-extractor-web-api -Dversion=4.1.0-SNAPSHOT -Dpackaging=jar
-mvn install:install-file -Dfile=./punkt-commons-ws-anno-1.0.0.jar -DgroupId=at.punkt.commons -DartifactId=punkt-commons-ws-anno -Dversion=1.0.0 -Dpackaging=jar
-
-
-
